a93c0b4b0157f0932a7c077f6fc9bcb2431a3804
Yahor Staravoitau <Yahor_Staravoitau@epam.com>
2017-03-03 10:22:18 +0300

    Start making PageFactory class.

---
+++ VoloteaFramework/Steps/PageFactory.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoloteaFramework.Steps
+{
+    public class PageFactory
+    {
+        protected IWebDriver Driver { get; set; }
+
+        public void Quit()
+        {
+            Driver.Quit();
+        }
+    }
+}
